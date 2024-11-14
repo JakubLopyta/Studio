5fa2c74cdebf1bb423b15b162deb44857f36b28f
Jakub Łopyta <30994@s.pm.szczecin.pl>
2024-11-14 15:52:44 +0100

    initial

---
+++ Migrations/20241113133445_Initial.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Studio.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Albums",
+                columns: table => new
+                {
+                    AlbumID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ReleaseDate = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Albums", x => x.AlbumID);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    AuthorID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.AuthorID);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    IsAdmin = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Username = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Email = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Password = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    FullName = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Adrdess = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserID);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Songs",
+                columns: table => new
+                {
+                    SongID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Duration = table.Column<int>(type: "int", nullable: false),
+                    AlbumID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Songs", x => x.SongID);
+                    table.ForeignKey(
+                        name: "FK_Songs_Albums_AlbumID",
+                        column: x => x.AlbumID,
+                        principalTable: "Albums",
+                        principalColumn: "AlbumID");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "AlbumAuthor",
+                columns: table => new
+                {
+                    AlbumsAlbumID = table.Column<int>(type: "int", nullable: false),
+                    AuthorsAuthorID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AlbumAuthor", x => new { x.AlbumsAlbumID, x.AuthorsAuthorID });
+                    table.ForeignKey(
+                        name: "FK_AlbumAuthor_Albums_AlbumsAlbumID",
+                        column: x => x.AlbumsAlbumID,
+                        principalTable: "Albums",
+                        principalColumn: "AlbumID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AlbumAuthor_Authors_AuthorsAuthorID",
+                        column: x => x.AuthorsAuthorID,
+                        principalTable: "Authors",
+                        principalColumn: "AuthorID",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "AuthorSong",
+                columns: table => new
+                {
+                    AuthorsAuthorID = table.Column<int>(type: "int", nullable: false),
+                    SongsSongID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthorSong", x => new { x.AuthorsAuthorID, x.SongsSongID });
+                    table.ForeignKey(
+                        name: "FK_AuthorSong_Authors_AuthorsAuthorID",
+                        column: x => x.AuthorsAuthorID,
+                        principalTable: "Authors",
+                        principalColumn: "AuthorID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AuthorSong_Songs_SongsSongID",
+                        column: x => x.SongsSongID,
+                        principalTable: "Songs",
+                        principalColumn: "SongID",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "SongUser",
+                columns: table => new
+                {
+                    OwnedSongsSongID = table.Column<int>(type: "int", nullable: false),
+                    UsersWhoOwnUserID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SongUser", x => new { x.OwnedSongsSongID, x.UsersWhoOwnUserID });
+                    table.ForeignKey(
+                        name: "FK_SongUser_Songs_OwnedSongsSongID",
+                        column: x => x.OwnedSongsSongID,
+                        principalTable: "Songs",
+                        principalColumn: "SongID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SongUser_User_UsersWhoOwnUserID",
+                        column: x => x.UsersWhoOwnUserID,
+                        principalTable: "User",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AlbumAuthor_AuthorsAuthorID",
+                table: "AlbumAuthor",
+                column: "AuthorsAuthorID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthorSong_SongsSongID",
+                table: "AuthorSong",
+                column: "SongsSongID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Songs_AlbumID",
+                table: "Songs",
+                column: "AlbumID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SongUser_UsersWhoOwnUserID",
+                table: "SongUser",
+                column: "UsersWhoOwnUserID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AlbumAuthor");
+
+            migrationBuilder.DropTable(
+                name: "AuthorSong");
+
+            migrationBuilder.DropTable(
+                name: "SongUser");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.DropTable(
+                name: "Songs");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "Albums");
+        }
+    }
+}
---
+++ Model.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Song> Songs { get; set; }
+    public DbSet<Album> Albums { get; set; }
+    public DbSet<Author> Authors{ get; set; }
+
+    public string ConnectionString { get; }
+    public MariaDbServerVersion ServerVersion { get; }
+
+    public BloggingContext()
+    {
+        ServerVersion = new MariaDbServerVersion(new Version(10, 4, 32));
+        ConnectionString = "server=localhost;user=root;password=;database=studio";
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseMySql(ConnectionString, ServerVersion);
+}
+
+public class Song
+{
+    public int SongID { get; set; }
+    public string Title { get; set; }
+    public int Duration {  get; set; }
+    public Album Album { get; set; }
+    public List<Author> Authors { get; set; }
+    public List<User> UsersWhoOwn { get; set; }
+}
+public class Album
+{
+    public int AlbumID { get; set; }
+    public string Title { get; set; }
+    public DateOnly ReleaseDate { get; set; } 
+    public List<Song> Songs { get; set; }
+    public List<Author> Authors { get; set; }
+}
+public class Author
+{
+    public int AuthorID { get; set; }
+    public string Name { get; set; }
+    public List<Album> Albums { get; set; }
+    public List<Song> Songs { get; set; }
+}
+public class User
+{
+    public int UserID { get; set; }
+    public bool IsAdmin { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string FullName { get; set; }
+    public string Address { get; set; }
+    public List<Song> OwnedSongs { get; set; }
+}
